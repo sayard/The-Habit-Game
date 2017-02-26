33bbb312d49f7ae8b30ba9616e602b440b6fb329
Karol Gracz <graczu42@gmail.com>
2017-02-26 21:20:04 +0100

    added new activity

---
+++ app/src/main/java/pl/c0/sayard/thehabitgame/AddNewHabitActivity.java
@@ -0,0 +1,13 @@
+package pl.c0.sayard.thehabitgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewHabitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_habit);
+    }
+}
