a01c19b6673d2a7d4a7d710b774c7f1df4cd1508
Karol Gracz <graczu42@gmail.com>
2017-03-29 21:54:54 +0200

    Added notifications template

---
+++ app/src/main/java/pl/c0/sayard/thehabitgame/utilities/NotificationReceiver.java
@@ -0,0 +1,35 @@
+package pl.c0.sayard.thehabitgame.utilities;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import pl.c0.sayard.thehabitgame.MainActivity;
+
+/**
+ * Created by Karol on 29.03.2017.
+ */
+
+public class NotificationReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent repeatingIntent = new Intent(context, MainActivity.class);
+        repeatingIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 101, repeatingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(android.R.drawable.arrow_up_float)
+                .setContentTitle("Title")
+                .setContentText("Text")
+                .setAutoCancel(true);
+
+        notificationManager.notify(101, builder.build());
+    }
+}
